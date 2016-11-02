3c1014a69d258505f7ae2ab002dca68ad70c0ff3
lava m <lavanymkanna@gmail.com>
2016-11-02 10:25:13 -0400

    removed restauth in settings

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -28,16 +28,14 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework.authtoken',
+
     'django.contrib.sites',
-    'rest_auth.registration',
     'core',
     'api',
     'user_profiles',
     'posts',
     'accounts',
-    'rest_auth',
+
 
 )
 
